0a333e5abba73f25e61237a6d3a272e41cc4de24
Tom Bray <tom.bray@student-cs.fr>
2022-04-11 00:08:47 +0200

    ajout de css bootstrap

--- viatrezor-front/src/components/Navbar.js
+++ viatrezor-front/src/components/Navbar.js
@@ -4,13 +4,12 @@ import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Button from "react-bootstrap/Button"
 import NavDropdown from "react-bootstrap/NavDropdown"
-import Form from "react-bootstrap/Form"
 import FormControl from "react-bootstrap/FormControl"
 
 function Navbarvt() {
   return (
     <div className = 'Navbarvt'>
-      <Navbar bg="bright" expand={false}>
+      <Navbar bg="dark" variant="dark" expand={false}>
         <Container fluid>
           <Navbar.Brand href="#">Menu</Navbar.Brand>
           <Navbar.Toggle aria-controls="Menu" />
@@ -20,14 +19,15 @@ function Navbarvt() {
             placement="end"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Chasse au Trézor</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="#action1">Accueil</Nav.Link>
                 <Nav.Link href="#action2">Enigme finale</Nav.Link>
-                <Nav.Link href="#action2">Classement</Nav.Link>
-                <Nav.Link href="#action2">Contact</Nav.Link>
+                <Nav.Link href="#action3">Classement</Nav.Link>
+                <Nav.Link href="#action4">Contact</Nav.Link>
+                <Nav.Link href="#authenticate">Se connecter</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
