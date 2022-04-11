d16af181ba15ccafc96de9f76cdfff02135a2d75
Deodat Pere <deodat.pere@student-cs.fr>
2022-04-11 16:09:42 +0200

    much nicer Leaderboard

--- viatrezor-front/src/components/App.js
+++ viatrezor-front/src/components/App.js
@@ -1,19 +1,25 @@
 import Banner from './Banner'
 import Navbarvt from './Navbar'
-import TeamLine from './Leaderboard'
+import Leaderboard from './Leaderboard'
 
+const team1 = {
+  name:'les BG du 97',
+  score: 1900,
+  time: 1215
+}
+const team2 = {
+  name: 'shreksophuckers',
+  score: 2500,
+  time: 1784
+}
+const teamList = [team1,team2];
 
 function App() { 
-  const team = {
-    name:'les BG du 97',
-    score: 1900,
-    time: 1215
-  }
   return (
   <div>
     <Banner />
     <Navbarvt />
-    <TeamLine team = {team}/>
+    <Leaderboard teamsList = {teamList}/>
   </div>
   )
 }
--- viatrezor-front/src/components/Leaderboard.js
+++ viatrezor-front/src/components/Leaderboard.js
@@ -1,9 +1,34 @@
+import '../styles/Leaderboard.css'
+
+function Leaderboard(props) {
+  const listTeams = props.teamsList.map((team) => <TeamLine team={team}/>)
+  return (
+    <div className='leaderboard'>
+      <div classname='leaderboard-head'>
+        <div>
+          <h1 className='leaderboard-head-text'>Leaderboard</h1>
+        </div>
+      </div>
+      <div classname='leaderboard-body'>{listTeams}</div>
+    </div>
+  )
+}
+
+function convertTime(time) {
+  var sec = time%60;
+  var min_tot = Math.floor(time/60);
+  var min = min_tot%60;
+  var hours = Math.floor(min_tot/60)
+  var result = hours+':'+min+':'+sec;
+  return (result)
+}
+
 function TeamLine(props) {
     return (
         <div className='team-line'>
           <div className='team-line-name'>{props.team.name}</div>
-          <div className='team-line-time'>{props.team.time}</div>
-          <div className='team-line-score'>{props.team.score}</div>
+          <div className='team-line-time'>{convertTime(props.team.time)}</div>
+          <div className='team-line-score'>{props.team.score}pts</div> 
           <StopButton name={props.team}/>
         </div>
       )
@@ -15,4 +40,4 @@ function StopButton(props) {
       )
 }
 
-export default TeamLine
+export default Leaderboard
