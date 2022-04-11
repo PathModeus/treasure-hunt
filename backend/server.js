89919ab32e000f24cd6ff146b9837b989c6c5095
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2022-04-11 17:10:00 +0200

    init routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,47 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+const http = require('http');
+const app = require('./app');
 
-app.get("/api/clients", (req, res) => {
-    const customers = [
-        { id: 1, firstName: "John", lastName: "Doe" },
-        { id: 2, firstName: "Abass", lastName: "Ben" },
-        { id: 3, firstName: "Toto", lastName: "Tata" }
-    ];
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-    res.json(customers);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
-app.listen(port, () => {
-    console.log(`listen on port ${port}`);
-});
+server.listen(port);
