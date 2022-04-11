adce47fee8aa8d2bc289ae03554a4ab6ee7207c1
El Yaagoubi Bilel <bilel.el-yaagoubi@student-cs.fr>
2022-04-11 19:48:44 +0200

    ajout team

--- viatrezor-front/src/components/App.js
+++ viatrezor-front/src/components/App.js
@@ -1,27 +1,32 @@
-import Banner from './Banner'
-import Navbarvt from './Navbar'
-import Leaderboard from './Leaderboard'
+import Banner from "./Banner";
+import Navbarvt from "./Navbar";
+import Leaderboard from "./Leaderboard";
 
 const team1 = {
-  name:'les BG du 97',
+  name: "les BG du 97",
   score: 1900,
-  time: 1215
-}
+  time: 1215,
+};
 const team2 = {
-  name: 'shreksophuckers',
+  name: "shreksophuckers",
   score: 2500,
-  time: 1784
-}
-const teamList = [team1,team2];
+  time: 1784,
+};
+const team3 = {
+  name: "Octoteam",
+  score: 8888888,
+  time: 8,
+};
+const teamList = [team1, team2, team3];
 
-function App() { 
+function App() {
   return (
-  <div>
-    <Banner />
-    <Navbarvt />
-    <Leaderboard teamsList = {teamList}/>
-  </div>
-  )
+    <div>
+      <Banner />
+      <Navbarvt />
+      <Leaderboard teamsList={teamList} />
+    </div>
+  );
 }
 
 export default App;
