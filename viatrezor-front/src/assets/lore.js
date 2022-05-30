82c5bd147fdc7a60209d08ce351a2998d1e68c0e
Tom Bray <tom.bray@student-cs.fr>
2022-05-30 11:50:47 +0200

    beginning of my work

--- viatrezor-front/src/assets/lore.js
+++ viatrezor-front/src/assets/lore.js
@@ -1,9 +1,9 @@
 const disconnectedTxt = "Bonjour, vous êtes là ? Connectez-vous afin que je puisse savoir qui vous êtes !"
 
-const notInTeamTxt = "Parfait ! Vous avez réussi à vous connecter à l'authentificateur de ViaRézo ! Je n'ai pas le temps pour les présentations, rendez-vous au carré des Sciences dès que possible pour former une équipe. J'ai besoin de votre aide en urgence !"
+const notInTeamTxt = "Parfait ! Vous avez réussi à vous connecter à l'authentificateur de ViaRézo ! Je n'ai pas le temps pour les présentations, rendez-vous au "
 
-const welcomingTxt = 'Bonjour équipe !'
+const welcomingTxt = "Bonjour équipe "
 
 const devTxt = "Bonjour ! L'interface administrateur pour gérer les équipes se trouve dans l'onglet leaderboard !"
 
-export { welcomingTxt, notInTeamTxt, devTxt, disconnectedTxt }
+export default Lore
--- viatrezor-front/src/components/Home.js
+++ viatrezor-front/src/components/Home.js
@@ -16,6 +16,33 @@ function Home(props) {
   )
 }
 
+function Advancement(props) {
+  const remainingTasks = props.nbEquipe - props.team.step
+  const completedTasks = props.team.step
+  const advancementBar = []
+  for (let i = 0; i < greenNumber; i++) {
+    advancementBar.push(<DoneStep />)
+  }
+  for (let i = 0; i < redNumber; i++) {
+    advancementBar.push(<ToDoStep />)
+  }
+  return (
+    <div className='advancement-bar'>{advancementBar}</div>
+  )
+}
+
+function ToDoStep() {
+  return (
+    <span className='red-circle circle'>&#128308;</span>
+  )
+}
+
+function DoneStep() {
+  return (
+    <span className='green-circle circle'>&#128994;</span>
+  )
+}
+
 
 const TypeWriter = ({ content = "", speed = 1000 }) => {
   const [displayedContent, setDisplayedContent] = useState("~% ");
@@ -27,7 +54,7 @@ const TypeWriter = ({ content = "", speed = 1000 }) => {
       setIndex((index) => {
         /*This setState function will set the index
         to index+1 if there is more content otherwise
-        it will destory this animation*/
+        it will destroy this animation*/
 
         if (index >= content.length - 1) {
           clearInterval(animKey);
