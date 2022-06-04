d3b91fbbe8eeb3c82940aae914ad578e4714372a
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2022-06-04 16:15:06 +0200

    ca marche toujours pas

--- viatrezor-back/app.js
+++ viatrezor-back/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const cors = require('cors')
 
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+app.use(cors())
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
 
 const bodyParser = require('body-parser');
 const config = require('./config.json');
--- viatrezor-front/src/components/Test.js
+++ viatrezor-front/src/components/Test.js
@@ -5,39 +5,41 @@ function TestRequest(setState, setResult) {
     console.log("passage 1");
     fetch('http://localhost:3001/api/15', {
         method: 'GET',
-        headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization', },
         mode: 'cors',
-        cache: 'default'
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
     })
         .then(function (res) {
-            console.log(res)
-            console.log('ici')
+            console.log(res);
+            console.log('ici');
+            console.log(res.body);
             setResult(
                 <div>
                     <h1>{res.body}</h1>
                 </div>
-            )
+            );
+            setState(true);
         }
         )
         .then(function () {
-            console.log('passé')
-            setState(true)
+            console.log('passé');
+            setState(true);
         })
-
-
 }
 
 function Test() {
     const [state, setState] = useState(false)
     const [result, setResult] = useState(<div></div>)
+    console.log('beep')
     return (
         <div className='authenticate-wrap'>
             <div className='authenticate'>
                 <h1 className='authenticate-text'>Connectez vous avec ViaRézo</h1>
                 <div className='authenticate-button-wrap'>
                     {state ? result :
-                        <form method="get" onClick={TestRequest(setState, setResult)}>
-                            <button className='authenticate-button' type="submit">Se connecter</button>
+                        <form onSubmit={() => TestRequest(setState, setResult)}>
+                            <button className='authenticate-button' type="submit">Send Request</button>
                         </form>
                     }
 
