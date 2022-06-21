e8dee012d170425e3137199f16f9ecd18486b352
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2022-06-21 19:21:26 +0200

    fixed activities table creation

--- viatrezor-back/src/diverse/bdd.js
+++ viatrezor-back/src/diverse/bdd.js
@@ -18,13 +18,18 @@ connection.query('CREATE TABLE IF NOT EXISTS individuals(id_indiv INTEGER PRIMAR
 connection.query('CREATE TABLE IF NOT EXISTS teams(team_id INTEGER PRIMARY KEY AUTO_INCREMENT, team_name TEXT NOT NULL, ongoing_activity TEXT NOT NULL, timer_status TINYINT DEFAULT "0", time INTEGER DEFAULT "0", timer_last_on DATETIME NOT NULL DEFAULT "2000-01-01T01:01:01", points INTEGER NOT NULL DEFAULT "0")');
 connection.query('CREATE TABLE IF NOT EXISTS players(id_player INTEGER PRIMARY KEY AUTO_INCREMENT, id_vr TEXT, team_id INTEGER DEFAULT "0", FOREIGN KEY(team_id) REFERENCES teams(team_id) ON DELETE SET DEFAULT)');
 connection.query('CREATE TABLE IF NOT EXISTS admins(id_admin INTEGER PRIMARY KEY AUTO_INCREMENT, id_vr TEXT, asso_name TEXT NOT NULL)');
-connection.query('CREATE TABLE IF NOT EXISTS activities(team_id INTEGER PRIMARY KEY AUTO_INCREMENT, activity_1 TINYINT NOT NULL DEFAULT "0", activity_2 TINYINT NOT NULL DEFAULT "0", activity_3 TINYINT NOT NULL DEFAULT "0", activity_4 TINYINT NOT NULL DEFAULT "0", activity_5 TINYINT NOT NULL DEFAULT "0", activity_6 TINYINT NOT NULL DEFAULT "0", activity_7 TINYINT NOT NULL DEFAULT "0"');
+connection.query('CREATE TABLE IF NOT EXISTS activities(team_id INTEGER PRIMARY KEY AUTO_INCREMENT, activity_1 TINYINT NOT NULL DEFAULT "0", activity_2 TINYINT NOT NULL DEFAULT "0", activity_3 TINYINT NOT NULL DEFAULT "0", activity_4 TINYINT NOT NULL DEFAULT "0", activity_5 TINYINT NOT NULL DEFAULT "0", activity_6 TINYINT NOT NULL DEFAULT "0", activity_7 TINYINT NOT NULL DEFAULT "0")');
 
 // INITIALISATION DE LA TABLE INDIVIDUALS
 //
 // Superadmins = Vrgens
 // Admins = 2As des autres assos
 // Reste = Joueurs
+//
+// L'idée est que les VRgens puissent remplacer n'importe quel staffeur / prêter leur PC au cas où il y ait un problème pour un staffeur
+// Une autre idée simple est de faire en sorte que tous les admins soient rentrés "à la main" lors de l'initialisation
+// et que n'importe quelle autre personne se connectant soit considérée comme joueur (Least privilege by default)
+// Cela pourrait également permettre à un 2A de venir aider une équipe qui serait en manque de joueurs ou de prêter son compte si un GPA ne l'a pas initialisé
 //
 // connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021berliouxqu', 'superadmin')");
 // connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021brayto', 'superadmin')");
@@ -43,7 +48,7 @@ connection.query('CREATE TABLE IF NOT EXISTS activities(team_id INTEGER PRIMARY 
 //
 // Par défaut, tout joueur se connectant se retrouvera dans l'équipe numéro 0 'Sans équipe', et devra changer d'équipe avant de pouvoir partir
 //
-// connection.query("INSERT INTO teams (team_id, team_name, ongoing_activity) VALUES (0, 'Sans équipe', 'En recherche d\'équipe')");
+// connection.query("INSERT INTO teams (team_id, team_name, ongoing_activity) VALUES (0, 'No team', 'Looking for a team')");
 //
 
 module.exports = connection;
