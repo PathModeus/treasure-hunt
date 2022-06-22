703edac3a1dfd4958e1a5596fe3a2f0d72e2b23f
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2022-06-22 22:36:55 +0200

    removed table individuals

--- viatrezor-back/src/diverse/bdd.js
+++ viatrezor-back/src/diverse/bdd.js
@@ -14,13 +14,12 @@ connection.connect()
 
 // INITIALISATION DE LA BDD :
 
-connection.query('CREATE TABLE IF NOT EXISTS individuals(id_indiv INTEGER PRIMARY KEY AUTO_INCREMENT, id_vr TEXT, role TEXT NOT NULL)');
 connection.query('CREATE TABLE IF NOT EXISTS teams(team_id INTEGER PRIMARY KEY AUTO_INCREMENT, team_name TEXT NOT NULL, ongoing_activity TEXT NOT NULL, timer_status TINYINT DEFAULT "0", time INTEGER DEFAULT "0", timer_last_on DATETIME NOT NULL DEFAULT "2000-01-01T01:01:01", points INTEGER NOT NULL DEFAULT "0")');
 connection.query('CREATE TABLE IF NOT EXISTS players(id_player INTEGER PRIMARY KEY AUTO_INCREMENT, id_vr TEXT, team_id INTEGER DEFAULT "0", FOREIGN KEY(team_id) REFERENCES teams(team_id) ON DELETE SET DEFAULT)');
 connection.query('CREATE TABLE IF NOT EXISTS admins(id_admin INTEGER PRIMARY KEY AUTO_INCREMENT, id_vr TEXT, asso_name TEXT NOT NULL)');
 connection.query('CREATE TABLE IF NOT EXISTS activities(team_id INTEGER PRIMARY KEY AUTO_INCREMENT, activity_1 TINYINT NOT NULL DEFAULT "0", activity_2 TINYINT NOT NULL DEFAULT "0", activity_3 TINYINT NOT NULL DEFAULT "0", activity_4 TINYINT NOT NULL DEFAULT "0", activity_5 TINYINT NOT NULL DEFAULT "0", activity_6 TINYINT NOT NULL DEFAULT "0", activity_7 TINYINT NOT NULL DEFAULT "0")');
 
-// INITIALISATION DE LA TABLE INDIVIDUALS
+// INITIALISATION DE LA TABLE ADMINS
 
 // Superadmins = Vrgens
 // Admins = 2As des autres assos
@@ -31,24 +30,9 @@ connection.query('CREATE TABLE IF NOT EXISTS activities(team_id INTEGER PRIMARY 
 // et que n'importe quelle autre personne se connectant soit considérée comme joueur (Least privilege by default)
 // Cela pourrait également permettre à un 2A de venir aider une équipe qui serait en manque de joueurs ou de prêter son compte si un GPA ne l'a pas initialisé
 //
-// Le tri des admins se dera via leur Asso.
+// Le tri des admins se fera via leur Asso.
 // Etre à ViaRézo donne tous les droits.
 // Etre ailleurs donne uniquement accès à l'épreuve de son club.
-
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021berliouxqu', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021brayto', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021perede', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021elyaagobi', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021augierme', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021gaudronan', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021kalflechju', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021bireem', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021meignanco', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021rosenberju', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021adjivonce', 'admin')");
-// connection.query("INSERT INTO individuals (id_vr, role) VALUES ('2021achghafan', 'admin')");
-
-// INITIALISATION DE LA TABLE ADMINS
 
 // connection.query("INSERT INTO admin (id_vr, asso) VALUES ('2021berliouxqu', 'VR')");
 // connection.query("INSERT INTO admin (id_vr, asso) VALUES ('2021brayto', 'VR')");
