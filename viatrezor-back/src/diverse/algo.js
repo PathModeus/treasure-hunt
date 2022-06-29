57e02444122d82b4d6f3cad1f39f569d5e1dad81
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2022-06-29 22:08:45 +0200

    init algo

--- viatrezor-back/src/diverse/algo.js
+++ viatrezor-back/src/diverse/algo.js
@@ -3,9 +3,20 @@
 // Ces fonctions devraient prendre en compte le nombre actuel d'équipes à l'épreuve ou en route.
 //
 
+const bdd = require('./bdd');
 
-function next_chall() {
-    return 1 + Math.floor(Math.random())
+function next_chall(team_id) {
+    bdd.query('SELECT * FROM activities WHERE team_id = (?)', [team_id], (err, rows, fields) => {
+        if (err) throw err
+        bdd.query('SELECT COUNT(*) FROM teams GROUP BY ongoing_activity', (err, rows, fields) => {
+            if (err) throw err
+
+            // itérer sur chacune des activités pour récupérer les activités non faites pour le moment
+            // Trouver parmi les actis pas encore faites, celle sur laquelle il y a le moins d'équipes à cet instant
+
+        })
+        return 1 + Math.floor(Math.random())
+    })
 }
 
-module.exports.next_chall = next_chall
+module.exports.next_chall = next_chall;
